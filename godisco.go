e7e213eb93fd97293e3088c66f07922951cf98a1
James Blizzard <james.blizzard@browserlondon.com>
2016-01-14 10:21:03 +0000

    Getsingle

--- godisco.go
+++ godisco.go
@@ -3,8 +3,17 @@ package godisco
 type Godisco struct {}
 
 func (gd *Godisco) Get(service string) (ips []string, err error) {
-
 	ips = append(ips, "10.10.10.9")
+	return
+}
 
+func (gd *Godisco) GetFirst(service string) (ip string, err error) {
+	ips, ipsErr := gd.Get(service)
+
+	if len(ips) > 0 && ipsErr != nil {
+		ip = ips[0]
+	} else {
+		err = ipsErr
+	}
 	return
 }
